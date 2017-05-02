9c5a846c9899d2a544ebc951805ccb47dd3b56d1
PDG <pdg102@naver.com>
2017-05-02 13:44:51 +0900

    Windows puts putc function add

--- ColorTextPrint.c
+++ ColorTextPrint.c
@@ -6,13 +6,68 @@ HANDLE getColorHandle()
 	return hConsole;
 }
 
-void w_applyColor(HANDLE i_handle, int i_color)
+void wApplyColor(HANDLE i_handle, int i_color)
 {
 	i_color %= 256;
 	SetConsoleTextAttribute(i_handle, i_color);
 }
 
+int wcPutc(char i_char, int i_color)
+{
+	HANDLE colorHandle;
+	int ret;
+
+	colorHandle = getColorHandle();
+	wApplyColor(colorHandle, i_color);
+	ret = putc(i_char, stdout);
+	wApplyColor(colorHandle, CT_PRINT_DEFAULT);
+	
+	return ret;
+}
+
+int wcPuts(char* i_str, int i_color)
+{
+	HANDLE colorHandle;
+	int ret;
+
+	colorHandle = getColorHandle();
+	wApplyColor(colorHandle, i_color);
+	ret = puts(i_str);
+	wApplyColor(colorHandle, CT_PRINT_DEFAULT);
+
+	return ret;
+}
+
 void l_applyColor(int i_color)
 {
-	puts("\x1B[1;31;40m");
+	puts("\x1B[31;40m");
+}
+
+void main()
+{
+	puts("HELLO");
+
+	wcPuts("BLACK", CT_PRINT_BLACK);
+	wcPuts("BLUE", CT_PRINT_BLUE);
+	wcPuts("GREEN", CT_PRINT_GREEN);
+	wcPuts("CYAN", CT_PRINT_CYAN);
+	wcPuts("RED", CT_PRINT_RED);
+	wcPuts("MAGENTA", CT_PRINT_MAGENTA);
+	wcPuts("ORANGE", CT_PRINT_ORANGE);
+	wcPuts("GRAY", CT_PRINT_GRAY);
+	wcPuts("DARKGRAY", CT_PRINT_DARKGRAY);
+	wcPuts("WHITE", CT_PRINT_WHITE);
+
+	wcPutc('1', CT_PRINT_BLACK);
+	wcPutc('1', CT_PRINT_BLUE);
+	wcPutc('1', CT_PRINT_GREEN);
+	wcPutc('1', CT_PRINT_CYAN);
+	wcPutc('1', CT_PRINT_RED);
+	wcPutc('1', CT_PRINT_MAGENTA);
+	wcPutc('1', CT_PRINT_ORANGE);
+	wcPutc('1', CT_PRINT_GRAY);
+	wcPutc('1', CT_PRINT_DARKGRAY);
+	wcPutc('1', CT_PRINT_WHITE);
+
+	getchar();
 }
--- ColorTextPrint.h
+++ ColorTextPrint.h
@@ -6,29 +6,43 @@
 #include <stdio.h>
 
 //Windows
-#ifdef _WIN32 || _WIN64
-#include <Windows.h>
+#ifdef _WIN32
 
-HANDLE getColorHandle();
-void w_applyColor(HANDLE, int);
-void colorPuts(char*);
+	#include <Windows.h>
+
+	#define CT_PRINT_BLACK 0
+	#define CT_PRINT_BLUE 9
+	#define CT_PRINT_GREEN 10
+	#define CT_PRINT_CYAN 11
+	#define CT_PRINT_RED 12
+	#define CT_PRINT_MAGENTA 13
+	#define CT_PRINT_ORANGE 14
+	#define CT_PRINT_GRAY 7
+	#define CT_PRINT_DARKGRAY 8
+	#define CT_PRINT_WHITE 15
+	#define CT_PRINT_DEFAULT CT_PRINT_GRAY
+
+	HANDLE getColorHandle();
+	void wApplyColor(HANDLE, int);
+	int wcPutc(char, int);
+	int wcPuts(char*, int);
+
 #endif
 
 //Linux or Unix
 #ifdef __linux__ || linux || __linux || __unix__ || unix || __unix
 
-#define CT_PRINT_RED 31
-#define CT_PRINT_GREEN 32
-#define CT_PRINT_YELLOW 33
-#define CT_PRINT_BLUE 34
-#define CT_PRINT_MAGENTA 35
-#define CT_PRINT_CYAN 36
-#define CT_PRINT_WHITE 37
-#define CT_PRINT_RESET 0
+	#define CT_PRINT_RED 31
+	#define CT_PRINT_GREEN 32
+	#define CT_PRINT_YELLOW 33
+	#define CT_PRINT_BLUE 34
+	#define CT_PRINT_MAGENTA 35
+	#define CT_PRINT_CYAN 36
+	#define CT_PRINT_WHITE 37
+	#define CT_PRINT_RESET 0
 
-void l_applyColor(int);
-
+	void l_applyColor(int);
+	
 #endif
-
 
 #endif
